bb201e475d1b0a6b3e7bc49b20a6ddcb0d969c88
tzcnt <trailingzerocount@gmail.com>
2024-01-25 20:37:06 -0800

    implement external_executor test

--- examples/external_awaitable.cpp
+++ examples/external_awaitable.cpp
@@ -1,7 +1,7 @@
 // A simple example that shows a tmc::task awaiting an external awaitable.
 // Since this external awaitable has no knowledge of TMC, it resumes the task on
-// its own thread. This is not an ideal way to integrate with TMC, but it proves
-// that it can work.
+// its own thread. If you wanted to move the task back to the original TMC
+// executor, you would have to do so manually using tmc::resume_on.
 
 // For the "proper" way to implement an external awaitable that resumes the
 // task resume back on its original TMC executor, see the implementation of
@@ -10,12 +10,24 @@
 
 #include <coroutine>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <thread>
 #include <utility>
 
 #define TMC_IMPL
-#include "external_executor.hpp"
 #include "tmc/all_headers.hpp"
+
+std::string this_thread_id() {
+  std::string tmc_tid = tmc::detail::this_thread::thread_name;
+  if (!tmc_tid.empty()) {
+    return tmc_tid;
+  } else {
+    static std::ostringstream id;
+    id << std::this_thread::get_id();
+    return "external thread " + id.str();
+  }
+}
 
 template <typename Result> class external_awaitable {
   Result result;
@@ -27,21 +39,22 @@ public:
 
   void await_suspend(std::coroutine_handle<> Outer) noexcept {
     outer = Outer;
-    external_executor().post([this]() {
+
+    std::thread([this]() {
       std::this_thread::sleep_for(std::chrono::seconds(1));
       result = 42;
       outer.resume();
-    });
+    }).detach();
   }
 
   Result await_resume() & noexcept { return result; }
   Result await_resume() && noexcept { return std::move(result); }
 };
 
-tmc::task<int> coro(external_awaitable<int>& Awaitable) {
+tmc::task<int> coro() {
   std::cout << "started on " << this_thread_id() << std::endl;
   std::cout << "co_awaiting..." << std::endl;
-  auto result = co_await Awaitable;
+  auto result = co_await external_awaitable<int>{};
   std::cout << "resumed on " << this_thread_id() << std::endl;
   if (result != 42) {
     std::printf("wrong result from external_awaitable\n");
@@ -51,9 +64,8 @@ tmc::task<int> coro(external_awaitable<int>& Awaitable) {
 
 int main() {
   tmc::cpu_executor().init();
-  external_awaitable<int> awaitable{};
   std::future<int> result_future =
-    tmc::post_waitable(tmc::cpu_executor(), coro(awaitable), 0);
+    tmc::post_waitable(tmc::cpu_executor(), coro(), 0);
   int result = result_future.get();
   if (result != 42) {
     std::printf("wrong result from result_future\n");
