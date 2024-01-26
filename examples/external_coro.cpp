99da6a52abcd4df0ddd1a358870f817c474505f8
tzcnt <trailingzerocount@gmail.com>
2024-01-25 23:01:21 -0800

    external_result_by_promise example

--- examples/external_coro.cpp
+++ examples/external_coro.cpp
@@ -50,8 +50,40 @@ void run_internal_first(int max) {
 //   }
 // }
 
+/// Another option to get a result from an external coroutine type is to
+/// manually capture a promise
+external_coro<void>
+external_result_by_promise(int Count, int Max, std::promise<int>&& promise) {
+  ++Count;
+  if (Count == Max) {
+    promise.set_value(Count);
+    co_return;
+  }
+  int result = co_await internal(Count, Max);
+  promise.set_value(result);
+  co_return;
+}
+
+void run_external_first_by_promise(int max) {
+  std::promise<int> result_promise;
+  std::future<int> result_future = result_promise.get_future();
+  // post_waitable doesn't work, as we have no way to deduce the result type of
+  // the external coro. by submitting it with post(), it will simply be treated
+  // as a void() functor and invoked (resumed) once.
+  tmc::post(
+    tmc::cpu_executor(),
+    external_result_by_promise(0, max, std::move(result_promise)), 0
+  );
+  int result = result_future.get();
+  if (result != max) {
+    std::printf("wrong result from result_future\n");
+  }
+}
+
 int main() {
   tmc::cpu_executor().init();
   run_internal_first(4);
   run_internal_first(5);
+  run_external_first_by_promise(4);
+  run_external_first_by_promise(5);
 }
