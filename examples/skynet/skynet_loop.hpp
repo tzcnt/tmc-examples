76ce345e9994b4a10ada3675821635c2b3ced1c6
tzcnt <trailingzerocount@gmail.com>
2024-04-09 21:43:12 -0700

    undo wait changes

--- examples/skynet/skynet_loop.hpp
+++ examples/skynet/skynet_loop.hpp
@@ -17,6 +17,7 @@ template <size_t Depth = 6> tmc::task<int> loop_skynet() {
   for (size_t j = 0; j < 5; ++j) {
     size_t waited_count = 0;
     auto execDur = std::chrono::nanoseconds(0);
+    auto startTime = std::chrono::high_resolution_clock::now();
     for (size_t i = 0; i < iter_count; ++i) {
       // Different implementations of skynet below. The most efficient
       // implementation, coro::bulk, is the default.
@@ -24,17 +25,14 @@ template <size_t Depth = 6> tmc::task<int> loop_skynet() {
       // co_await skynet::direct::skynet<Depth>();
       // co_await skynet::func::single::skynet<Depth>();
       // co_await skynet::coro::single::skynet<Depth>();
-      auto startTime = std::chrono::high_resolution_clock::now();
       co_await skynet::coro::bulk::skynet<Depth>();
-      auto endTime = std::chrono::high_resolution_clock::now();
-      execDur += (endTime - startTime);
-      waited_count +=
-        tmc::test::wait_for_all_threads_to_sleep(tmc::cpu_executor());
 
       // co_await skynet::braids::single::skynet<Depth>();
       // co_await skynet::braids::fork::skynet<Depth>();
       // co_await skynet::braids::bulk::skynet<Depth>();
     }
+    auto endTime = std::chrono::high_resolution_clock::now();
+    execDur += (endTime - startTime);
     auto execPrint =
       std::chrono::duration_cast<std::chrono::microseconds>(execDur);
     std::printf(
