693dc41fff44d8c7cbc2b4911c4e0f89c58fa2e7
tzcnt <trailingzerocount@gmail.com>
2024-12-20 22:25:15 -0800

    tuple test

--- examples/fib.cpp
+++ examples/fib.cpp
@@ -9,6 +9,7 @@
 #include <chrono>
 #include <cinttypes>
 #include <cstdio>
+#include <ranges>
 
 using namespace tmc;
 
@@ -40,10 +41,14 @@ static task<size_t> fib(size_t n) {
   //                                         .begin());
   // co_return results[0] + results[1];
 
+  auto tasks = co_await spawn_tuple(fib(n - 1), fib(n - 2));
+  auto x = std::get<0>(tasks);
+  auto y = std::get<1>(tasks);
+
   /* Spawn one, then serially execute the other, then await the first */
-  auto xt = spawn(fib(n - 1)).run_early();
-  auto y = co_await fib(n - 2);
-  auto x = co_await std::move(xt);
+  // auto xt = spawn(fib(n - 1)).run_early();
+  // auto y = co_await fib(n - 2);
+  // auto x = co_await std::move(xt);
   co_return x + y;
 }
 
