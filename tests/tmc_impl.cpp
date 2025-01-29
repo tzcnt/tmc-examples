3aaea08ed9f2c78d127bf4270b6a440cb7131a63
tzcnt <trailingzerocount@gmail.com>
2025-01-29 07:15:14 -0800

    setup gtest

---
+++ tests/tmc_impl.cpp
@@ -0,0 +1,6 @@
+// Provide the implementation for TMC functions here via TMC_IMPL
+// Other files can just include the headers
+
+#define TMC_IMPL
+
+#include "tmc/all_headers.hpp" // IWYU pragma: keep
