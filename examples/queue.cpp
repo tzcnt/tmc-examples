5ffb361dec4751576f8e074de2575f05ea1f0b37
tzcnt <trailingzerocount@gmail.com>
2025-02-24 07:48:01 -0800

    update queue example

--- examples/queue.cpp
+++ examples/queue.cpp
@@ -1,68 +1,93 @@
 #define TMC_IMPL
 
 #include "tmc/all_headers.hpp"
-#include "tmc/fixed_queue.hpp"
+#include "tmc/ticket_queue.hpp"
 
 #include <cstdio>
+#include <numeric>
 
-#define NELEMS 1000
-#define QUEUE_SIZE 512
+#define NELEMS 10000
+#define QUEUE_SIZE 1000
+
+using tmc::queue_error::CLOSED;
+using tmc::queue_error::OK;
 
 template <size_t Size>
-tmc::task<void> producer(tmc::fixed_queue<int, Size>& q, size_t count) {
+tmc::task<void> producer(tmc::ticket_queue<int, Size>& q, size_t count) {
   for (size_t i = 0; i < count; ++i) {
-    auto err = co_await q.push(i);
+    auto err = q.push(i);
     assert(!err);
   }
+  co_return;
 }
 
 template <size_t Size>
-tmc::task<void> consumer(tmc::fixed_queue<int, Size>& q) {
+tmc::task<size_t> consumer(tmc::ticket_queue<int, Size>& q) {
+  size_t count = 0;
   auto data = co_await q.pull();
 
   while (data.index() == OK) {
+    ++count;
     // std::printf("%d ", std::get<0>(data));
+    // std::fflush(stdout);
     data = co_await q.pull();
   }
   // queue should be closed, not some other error
   assert(data.index() == CLOSED);
+  co_return count;
 }
 
 int main() {
   tmc::cpu_executor().init();
   return tmc::async_main([]() -> tmc::task<int> {
-    for (size_t prodCount = 1; prodCount <= 10; ++prodCount) {
-      for (size_t consCount = 1; consCount <= 10; ++consCount) {
-        tmc::fixed_queue<int, QUEUE_SIZE> q;
-        size_t per_task = NELEMS / prodCount;
-        size_t rem = NELEMS % prodCount;
-        std::vector<tmc::task<void>> prod(prodCount);
-        for (size_t i = 0; i < prodCount; ++i) {
-          size_t count = i < rem ? per_task + 1 : per_task;
-          prod[i] = producer(q, count);
+    for (size_t i = 0; i < 10; ++i)
+      for (size_t prodCount = 10; prodCount <= 10; ++prodCount) {
+        for (size_t consCount = 10; consCount <= 10; ++consCount) {
+          tmc::ticket_queue<int, QUEUE_SIZE> q;
+          size_t per_task = NELEMS / prodCount;
+          size_t rem = NELEMS % prodCount;
+          std::vector<tmc::task<void>> prod(prodCount);
+          for (size_t i = 0; i < prodCount; ++i) {
+            size_t count = i < rem ? per_task + 1 : per_task;
+            prod[i] = producer(q, count);
+          }
+          std::vector<tmc::task<size_t>> cons(consCount);
+          for (size_t i = 0; i < consCount; ++i) {
+            cons[i] = consumer(q);
+          }
+
+          auto startTime = std::chrono::high_resolution_clock::now();
+          auto c = tmc::spawn_many(cons.data(), cons.size()).run_early();
+          co_await tmc::spawn_many(prod.data(), prod.size());
+          // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+          q.close();
+          // std::printf(
+          //   "allocs: %zu\tfails: %zu\n", q.allocs.load(), q.fails.load()
+          // );
+          auto counts = co_await std::move(c);
+
+          auto endTime = std::chrono::high_resolution_clock::now();
+
+          size_t total = std::accumulate(
+            counts.begin(), counts.end(), static_cast<size_t>(0)
+          );
+          if (total != NELEMS) {
+            std::printf(
+              "FAIL: Expected %zu elements but consumed %zu elements\n", total,
+              static_cast<size_t>(NELEMS)
+            );
+          }
+
+          size_t execDur =
+            std::chrono::duration_cast<std::chrono::microseconds>(
+              endTime - startTime
+            )
+              .count();
+          std::printf(
+            "%zu prod\t%zu cons\t %zu us\n", prodCount, consCount, execDur
+          );
         }
-        std::vector<tmc::task<void>> cons(consCount);
-        for (size_t i = 0; i < consCount; ++i) {
-          cons[i] = consumer(q);
-        }
-
-        auto startTime = std::chrono::high_resolution_clock::now();
-        auto c = tmc::spawn_many(cons.data(), cons.size()).run_early();
-        co_await tmc::spawn_many(prod.data(), prod.size());
-        q.close();
-        co_await std::move(c);
-
-        auto endTime = std::chrono::high_resolution_clock::now();
-
-        size_t execDur = std::chrono::duration_cast<std::chrono::microseconds>(
-                           endTime - startTime
-        )
-                           .count();
-        std::printf(
-          "%zu prod\t%zu cons\t %zu us\n", prodCount, consCount, execDur
-        );
       }
-    }
     co_return 0;
   }());
 }
