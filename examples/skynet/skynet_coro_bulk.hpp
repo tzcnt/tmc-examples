385afeec253f904c3a1fbb530b3dfa5bff636335
tzcnt <trailingzerocount@gmail.com>
2025-03-14 16:23:35 -0700

    only prevent migration on depth - 1

--- examples/skynet/skynet_coro_bulk.hpp
+++ examples/skynet/skynet_coro_bulk.hpp
@@ -37,18 +37,18 @@ tmc::task<size_t> skynet_one(size_t BaseNum, size_t Depth) {
   // tmc::spawn_many<10>(children.data());
 
   /// Construction from a sized iterator has slightly better performance
-  auto ts = tmc::spawn_many<10>(
+  auto tasks = tmc::spawn_many<10>(
     (std::ranges::views::iota(0UL) |
      std::ranges::views::transform([=](size_t idx) {
        return skynet_one<DepthMax>(BaseNum + depthOffset * idx, Depth + 1);
      })
     ).begin()
   );
-  // Prevent thread migration on lowest 2 levels (100 tasks per)
-  if (Depth >= DepthMax - 2) {
-    ts.set_thread_hint(tmc::detail::this_thread::thread_index);
+  // Prevent thread migration on leaf nodes (10 tasks per)
+  if (Depth >= DepthMax - 1) {
+    tasks.set_thread_hint(tmc::detail::this_thread::thread_index);
   }
-  std::array<size_t, 10> results = co_await std::move(ts);
+  std::array<size_t, 10> results = co_await std::move(tasks);
 
   for (size_t idx = 0; idx < 10; ++idx) {
     count += results[idx];
