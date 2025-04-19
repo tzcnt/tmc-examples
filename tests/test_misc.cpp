98d0d1f255a1dc14291f61f924e2fad652a1d246
tzcnt <trailingzerocount@gmail.com>
2025-04-18 17:39:14 -0700

    add tests

---
+++ tests/test_misc.cpp
@@ -0,0 +1,113 @@
+// Various tests to increase code coverage in specific areas that are otherwise
+// not exercised.
+
+#include "atomic_awaitable.hpp"
+#include "test_common.hpp"
+#include "tmc/external.hpp"
+#include "tmc/sync.hpp"
+
+#include <gtest/gtest.h>
+
+#define CATEGORY test_misc
+
+class CATEGORY : public testing::Test {
+protected:
+  static void SetUpTestSuite() {
+    tmc::cpu_executor().set_thread_count(4).init();
+  }
+
+  static void TearDownTestSuite() { tmc::cpu_executor().teardown(); }
+
+  static tmc::ex_cpu& ex() { return tmc::cpu_executor(); }
+};
+
+TEST_F(CATEGORY, qu_lockfree_expand_implicit_producer_index) {
+  auto t1 = tmc::post_bulk_waitable(
+    ex(), tmc::iter_adapter(0, [](int i) -> tmc::task<void> { co_return; }),
+    8000
+  );
+
+  auto t2 = tmc::post_bulk_waitable(
+    ex(), tmc::iter_adapter(0, [](int i) -> tmc::task<void> { co_return; }),
+    32000
+  );
+  t1.wait();
+  t2.wait();
+}
+
+TEST_F(CATEGORY, qu_lockfree_expand_explicit_producer_index) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    auto t1 =
+      tmc::spawn_many(
+        tmc::iter_adapter(0, [](int i) -> tmc::task<void> { co_return; }), 8000
+      )
+        .fork();
+    co_await tmc::spawn_many(
+      tmc::iter_adapter(0, [](int i) -> tmc::task<void> { co_return; }), 32000
+    );
+    co_await std::move(t1);
+  }());
+}
+
+TEST_F(CATEGORY, qu_inbox_try_push_bulk) {
+  auto t1 = tmc::post_bulk_waitable(
+    ex(), tmc::iter_adapter(0, [](int i) -> tmc::task<void> { co_return; }),
+    32000, 0, 0
+  );
+  t1.wait();
+}
+
+TEST_F(CATEGORY, qu_inbox_try_push_full) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    atomic_awaitable<int> aa(0, 32000);
+    for (size_t i = 0; i < 32000; ++i) {
+      tmc::post(ex(), [](atomic_awaitable<int>& AA) -> tmc::task<void> {
+        ++AA.ref();
+        AA.ref().notify_all();
+        co_return;
+      }(aa));
+    }
+    co_await aa;
+  }());
+}
+
+TEST_F(CATEGORY, post_checked_default_executor) {
+  tmc::set_default_executor(ex());
+
+  test_async_main(ex(), []() -> tmc::task<void> {
+    atomic_awaitable<int> aa(0, 1);
+    tmc::detail::post_checked(
+      nullptr,
+      [](atomic_awaitable<int>& AA) -> tmc::task<void> {
+        ++AA.ref();
+        AA.ref().notify_all();
+        co_return;
+      }(aa)
+    );
+    co_await aa;
+  }());
+
+  tmc::set_default_executor(nullptr);
+}
+
+TEST_F(CATEGORY, post_bulk_checked_default_executor) {
+  tmc::set_default_executor(ex());
+
+  test_async_main(ex(), []() -> tmc::task<void> {
+    atomic_awaitable<int> aa(0, 2);
+    std::array<std::coroutine_handle<>, 2> tasks;
+    for (size_t i = 0; i < tasks.size(); ++i) {
+      tasks[i] = [](atomic_awaitable<int>& AA) -> tmc::task<void> {
+        ++AA.ref();
+        AA.ref().notify_all();
+        co_return;
+      }(aa);
+    }
+    tmc::detail::post_bulk_checked(nullptr, tasks.data(), tasks.size());
+    co_await aa;
+  }());
+
+  tmc::set_default_executor(nullptr);
+}
+
+#undef CATEGORY
---
+++ tests/test_thread_layout.cpp
@@ -0,0 +1,70 @@
+// Various tests to increase code coverage in specific areas that are otherwise
+// not exercised.
+#include "test_common.hpp"
+#include "tmc/detail/thread_layout.hpp"
+
+#include <gtest/gtest.h>
+
+#define CATEGORY test_thread_layout
+
+class CATEGORY : public testing::Test {
+protected:
+  static void SetUpTestSuite() {
+    tmc::cpu_executor().set_thread_count(4).init();
+  }
+
+  static void TearDownTestSuite() { tmc::cpu_executor().teardown(); }
+
+  static tmc::ex_cpu& ex() { return tmc::cpu_executor(); }
+};
+
+TEST_F(CATEGORY, group_iteration_order) {
+  tmc::detail::get_group_iteration_order(16, 5);
+}
+
+std::vector<tmc::detail::L3CacheSet> get_core_groups() {
+  std::vector<tmc::detail::L3CacheSet> groupedCores;
+  for (size_t i = 0; i < 16; ++i) {
+    groupedCores.push_back(
+      tmc::detail::L3CacheSet{nullptr, 4, std::vector<size_t>{}}
+    );
+  }
+  return groupedCores;
+}
+
+TEST_F(CATEGORY, get_matrixes) {
+  auto groupedCores = get_core_groups();
+  size_t threadCount = 0;
+  for (size_t i = 0; i < groupedCores.size(); ++i) {
+    threadCount += groupedCores[i].group_size;
+  }
+  {
+    std::vector<size_t> steal_matrix =
+      tmc::detail::get_lattice_matrix(groupedCores);
+    EXPECT_EQ(steal_matrix.size(), threadCount * threadCount);
+    auto waker_matrix = tmc::detail::invert_matrix(steal_matrix, threadCount);
+    EXPECT_EQ(waker_matrix.size(), threadCount * threadCount);
+
+    // tmc::detail::print_square_matrix(steal_matrix, threadCount);
+  }
+  {
+    std::vector<size_t> steal_matrix =
+      tmc::detail::get_hierarchical_matrix(groupedCores);
+    EXPECT_EQ(steal_matrix.size(), threadCount * threadCount);
+    auto waker_matrix = tmc::detail::invert_matrix(steal_matrix, threadCount);
+    EXPECT_EQ(waker_matrix.size(), threadCount * threadCount);
+  }
+}
+
+#ifdef TMC_USE_HWLOC
+TEST_F(CATEGORY, core_group_resize_no_change) {
+  auto groupedCores = get_core_groups();
+  auto sz = groupedCores.size();
+  bool lasso = false;
+  tmc::detail::adjust_thread_groups(64, 0.0f, groupedCores, lasso);
+  EXPECT_EQ(lasso, true);
+  EXPECT_EQ(groupedCores.size(), sz);
+}
+#endif
+
+#undef CATEGORY
