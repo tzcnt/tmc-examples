a71d4271c3af66e7659b7acf670f5307f9c05b28
tzcnt <trailingzerocount@gmail.com>
2025-05-04 09:56:45 -0700

    add mutex tests

---
+++ tests/test_mutex.cpp
@@ -0,0 +1,109 @@
+#include "atomic_awaitable.hpp"
+#include "test_common.hpp"
+#include "tmc/mutex.hpp"
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <thread>
+
+#define CATEGORY test_mutex
+
+class CATEGORY : public testing::Test {
+protected:
+  static void SetUpTestSuite() {
+    tmc::cpu_executor().set_thread_count(4).init();
+  }
+
+  static void TearDownTestSuite() { tmc::cpu_executor().teardown(); }
+
+  static tmc::ex_cpu& ex() { return tmc::cpu_executor(); }
+};
+
+TEST_F(CATEGORY, nonblocking) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    tmc::mutex mut;
+    EXPECT_EQ(mut.is_locked(), false);
+    co_await mut;
+    EXPECT_EQ(mut.is_locked(), true);
+    mut.unlock();
+    EXPECT_EQ(mut.is_locked(), false);
+  }());
+}
+
+TEST_F(CATEGORY, one_waiter) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    tmc::mutex mut;
+    co_await mut;
+    EXPECT_EQ(mut.is_locked(), true);
+
+    atomic_awaitable<int> aa(1);
+    auto t =
+      tmc::spawn(
+        [](tmc::mutex& Mut, atomic_awaitable<int>& AA) -> tmc::task<void> {
+          co_await Mut;
+          AA.inc();
+        }(mut, aa)
+      )
+        .fork();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    EXPECT_EQ(mut.is_locked(), true);
+    EXPECT_EQ(aa.load(), 0);
+    mut.unlock();
+    co_await aa;
+    co_await std::move(t);
+  }());
+}
+
+TEST_F(CATEGORY, multi_waiter) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    tmc::mutex mut;
+    co_await mut;
+    EXPECT_EQ(mut.is_locked(), true);
+
+    atomic_awaitable<int> aa(5);
+    std::array<tmc::task<void>, 5> tasks;
+    for (size_t i = 0; i < 5; ++i) {
+      tasks[i] =
+        [](tmc::mutex& Mut, atomic_awaitable<int>& AA) -> tmc::task<void> {
+        co_await Mut;
+        AA.inc();
+        Mut.unlock();
+      }(mut, aa);
+    }
+    auto t = tmc::spawn_many(tasks).fork();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    EXPECT_EQ(mut.is_locked(), true);
+    EXPECT_EQ(aa.load(), 0);
+    mut.unlock();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    co_await aa;
+    EXPECT_EQ(aa.load(), 5);
+    co_await std::move(t);
+  }());
+}
+
+TEST_F(CATEGORY, resume_in_destructor) {
+  test_async_main(ex(), []() -> tmc::task<void> {
+    atomic_awaitable<int> aa(1);
+    std::optional<tmc::mutex> mut;
+    mut.emplace();
+    co_await *mut;
+    auto t =
+      tmc::spawn(
+        [](tmc::mutex& Mut, atomic_awaitable<int>& AA) -> tmc::task<void> {
+          co_await Mut;
+          AA.inc();
+        }(*mut, aa)
+      )
+        .fork();
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    EXPECT_EQ(aa.load(), 0);
+    // Destroy mut while the task is still waiting.
+    mut.reset();
+    co_await aa;
+    co_await std::move(t);
+  }());
+}
+
+#undef CATEGORY
