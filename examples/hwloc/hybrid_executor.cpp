ef99046574387326c8a053b578ea694e7520a5e6
tzcnt <trailingzerocount@gmail.com>
2025-12-27 17:28:32 -0800

    update hybrid_executor example

--- examples/hwloc/hybrid_executor.cpp
+++ examples/hwloc/hybrid_executor.cpp
@@ -23,6 +23,7 @@ static void print_location() {
 
 int main() {
   auto topo = tmc::topology::query();
+
   if (topo.is_hybrid()) {
     std::printf("Hybrid CPU detected:\n");
     std::printf("  Performance cores: %zu\n", topo.cpu_kind_counts[0]);
@@ -35,19 +36,25 @@ int main() {
     e_cores.set_cpu_kinds(tmc::topology::cpu_kind::EFFICIENCY1);
     tmc::cpu_executor()
       .add_partition(p_cores, 0, 1)
-      .add_partition(e_cores, 1, 2);
+      .add_partition(e_cores, 1, 2)
+      .set_priority_count(2);
   } else {
     std::printf("Homogeneous CPU: %zu cores\n", topo.cpu_kind_counts[0]);
     // Priority 0 and 1 will run on any core
+    tmc::cpu_executor().set_priority_count(2);
+    // Optionally, you could set_priority_count(1) here, so that on a
+    // homogeneous processor, all work runs at the same priority. If work is
+    // submitted at priority 1, it would be clamped to 0.
   }
+
   tmc::cpu_executor()
     .set_thread_init_hook([](tmc::topology::thread_info info) {
       thread_name = info.group.cpu_kind == tmc::topology::cpu_kind::PERFORMANCE
                       ? "performance"
                       : "efficiency";
     })
-    .set_priority_count(2)
     .init();
+
   std::printf("\nrunning high priority task...\n");
   tmc::post_waitable(tmc::cpu_executor(), print_location, 0).wait();
   std::printf("\nrunning low priority task...\n");
