272c491c343fe01e4cd82aa0eae5ddcde30dd912
tzcnt <trailingzerocount@gmail.com>
2026-05-16 10:49:41 -0700

    add to chan_bench

--- examples/chan_bench.cpp
+++ examples/chan_bench.cpp
@@ -1,30 +1,22 @@
-// A benchmark for the throughput of tmc::chan.
+// A benchmark for the throughput of tmc::mutex_queue.
 // Sweeps from 1 to 10 producers and 1 to 10 consumers.
 
 #include "tmc/all_headers.hpp"
 
-#include <cassert>
 #include <chrono>
 #include <cstdio>
+#include <optional>
 #include <string>
 #include <utility>
 #include <vector>
 
 #define NELEMS 10000000
 
-struct chan_config : tmc::chan_default_config {
-  // static inline constexpr size_t BlockSize = 4096;
-  // static inline constexpr size_t PackingLevel = 0;
-  // static inline constexpr bool EmbedFirstBlock = false;
-};
-using token = tmc::chan_tok<size_t, chan_config>;
+using queue_t = tmc::mutex_queue<std::optional<size_t>>;
 
-static tmc::task<void> producer(token chan, size_t count, size_t base) {
-  // It would be more efficient to call `chan.post_bulk()`,
-  // but for this benchmark we test pushing 1 at a time.
+static tmc::task<void> producer(queue_t* queue, size_t count, size_t base) {
   for (size_t i = 0; i < count; ++i) {
-    [[maybe_unused]] bool ok = co_await chan.push(base + i);
-    assert(ok);
+    co_await queue->push_back(base + i);
   }
 }
 
@@ -33,37 +25,15 @@ struct result {
   size_t sum;
 };
 
-static tmc::task<result> consumer(token chan) {
+static tmc::task<result> consumer(queue_t* queue) {
   size_t count = 0;
   size_t sum = 0;
 
-  // pull() implementation
-  while (auto data = co_await chan.pull()) {
+  while (auto data = co_await queue->pop_front()) {
     ++count;
     sum += *data;
   }
   co_return result{count, sum};
-
-  // // try_pull() implementation
-  // while (true) {
-  //   auto data = chan.try_pull();
-  //   switch (data.index()) {
-  //   case tmc::chan_err::OK:
-  //     ++count;
-  //     sum += std::get<tmc::chan_err::OK>(data);
-  //     break;
-  //   case tmc::chan_err::EMPTY:
-  //     // Spinning on try_pull() without yielding from an executor thread can
-  //     // cause deadlock if producers are waiting to run. Calling reschedule()
-  //     // ensures that those producers are able to run.
-  //     co_await tmc::reschedule();
-  //     break;
-  //   case tmc::chan_err::CLOSED:
-  //     co_return result{count, sum};
-  //   default:
-  //     break;
-  //   }
-  // }
 }
 
 static std::string formatWithCommas(size_t n) {
@@ -79,7 +49,7 @@ static std::string formatWithCommas(size_t n) {
 int main() {
   tmc::cpu_executor().init();
   std::printf(
-    "chan_bench: %zu threads | %s elements\n",
+    "queue_bench: %zu threads | %s elements\n",
     tmc::cpu_executor().thread_count(), formatWithCommas(NELEMS).c_str()
   );
   return tmc::async_main([]() -> tmc::task<int> {
@@ -87,28 +57,27 @@ int main() {
 
     for (size_t consCount = 1; consCount <= 10; ++consCount) {
       for (size_t prodCount = 1; prodCount <= 10; ++prodCount) {
-        auto chan = tmc::make_channel<size_t, chan_config>();
+        queue_t queue;
         size_t per_task = NELEMS / prodCount;
         size_t rem = NELEMS % prodCount;
         std::vector<tmc::task<void>> prod(prodCount);
         size_t base = 0;
         for (size_t i = 0; i < prodCount; ++i) {
           size_t count = i < rem ? per_task + 1 : per_task;
-          prod[i] = producer(chan, count, base);
+          prod[i] = producer(&queue, count, base);
           base += count;
         }
         std::vector<tmc::task<result>> cons(consCount);
         for (size_t i = 0; i < consCount; ++i) {
-          cons[i] = consumer(chan);
+          cons[i] = consumer(&queue);
         }
         auto startTime = std::chrono::high_resolution_clock::now();
         auto c = tmc::spawn_many(cons).fork();
         co_await tmc::spawn_many(prod);
 
-        // The call to close() is not necessary, but is included here for
-        // exposition.
-        chan.close();
-        co_await chan.drain();
+        for (size_t i = 0; i < consCount; ++i) {
+          co_await queue.push_back(std::nullopt);
+        }
         auto consResults = co_await std::move(c);
 
         auto endTime = std::chrono::high_resolution_clock::now();
