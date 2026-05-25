5e6be86ef18b50fae4ea66c609609ae0e96b793e
tzcnt <trailingzerocount@gmail.com>
2026-05-25 08:54:57 -0700

    generalize the queue bench to MPSC/SPSC

--- examples/qu_mpsc_bench.cpp
+++ examples/qu_mpsc_bench.cpp
@@ -1,9 +1,6 @@
-// A benchmark for the throughput of tmc::qu_unbounded_mpsc in SPSC mode.
+// A benchmark for the throughput of tmc::qu_unbounded_spsc / mpsc.
 
 #include "tmc/all_headers.hpp"
-#include "tmc/aw_yield.hpp"
-#include "tmc/qu_unbounded_mpsc.hpp"
-#include "tmc/topology.hpp"
 
 #include <cassert>
 #include <chrono>
@@ -13,9 +10,22 @@
 #include <vector>
 
 #define NELEMS 10000000
-static constexpr size_t PRODUCER_COUNT = 2;
-static constexpr size_t CONSUMER_COUNT = 1;
+#define PRODUCER_COUNT 1
+#define CONSUMER_COUNT 1
 
+static_assert(
+  CONSUMER_COUNT == 1, "This benchmark requires a single consumer."
+);
+
+#if PRODUCER_COUNT == 1
+struct queue_config : tmc::qu_unbounded_spsc_default_config {
+  //  static inline constexpr bool ConsumerCanSuspend = true;
+  //  static inline constexpr size_t BlockSize = 4096;
+  //  static inline constexpr size_t PackingLevel = 1;
+  //  static inline constexpr bool EmbedFirstBlock = false;
+};
+using queue_t = tmc::qu_unbounded_spsc<size_t, queue_config>;
+#else
 struct queue_config : tmc::qu_unbounded_mpsc_default_config {
   //  static inline constexpr bool ConsumerCanSuspend = true;
   //  static inline constexpr size_t BlockSize = 4096;
@@ -23,6 +33,7 @@ struct queue_config : tmc::qu_unbounded_mpsc_default_config {
   //  static inline constexpr bool EmbedFirstBlock = false;
 };
 using queue_t = tmc::qu_unbounded_mpsc<size_t, queue_config>;
+#endif
 
 struct producer_result {
   size_t duration_us;
@@ -35,9 +46,6 @@ producer(queue_t& queue, size_t count, size_t base) {
   // but for this benchmark we test pushing 1 at a time.
   for (size_t i = 0; i < count; ++i) {
     queue.post(base + i);
-    if (i % 16384 == 0) {
-      co_await tmc::reschedule();
-    }
   }
   auto endTime = std::chrono::high_resolution_clock::now();
   co_return producer_result{static_cast<size_t>(
@@ -76,6 +84,7 @@ consumer_try_pull(queue_t& queue, size_t expected_count) {
       ++count;
       sum += *data;
     } else {
+      TMC_CPU_PAUSE();
       co_await tmc::reschedule();
     }
   }
@@ -94,90 +103,105 @@ static std::string formatWithCommas(size_t n) {
 }
 
 int main() {
+  tmc::ex_cpu_st prodEx;
 #ifdef TMC_USE_HWLOC
+  // Pin producer to physical core 0
   tmc::topology::topology_filter f;
-  f.set_group_indexes({0});
-  tmc::cpu_executor().add_partition(f);
+  f.set_core_indexes({0});
+  prodEx.add_partition(f);
 #endif
-  tmc::cpu_executor().init();
+  prodEx.init();
   std::printf(
-    "qu_mpsc SPSC bench: %zu threads | %s elements\n",
-    tmc::cpu_executor().thread_count(), formatWithCommas(NELEMS).c_str()
+    "qu_mpsc SPSC bench: %s elements\n", formatWithCommas(NELEMS).c_str()
   );
-  return tmc::async_main([]() -> tmc::task<int> {
-    auto overallStart = std::chrono::high_resolution_clock::now();
+  return tmc::post_waitable(
+           prodEx,
+           []() -> tmc::task<int> {
+             tmc::ex_cpu_st consEx;
+#ifdef TMC_USE_HWLOC
+             // Pin consumer to physical core 1
+             tmc::topology::topology_filter f2;
+             f2.set_core_indexes({1});
+             consEx.add_partition(f2);
+#endif
+             consEx.init();
 
-    queue_t queue;
-    size_t per_task = NELEMS / PRODUCER_COUNT;
-    size_t rem = NELEMS % PRODUCER_COUNT;
-    std::vector<tmc::task<producer_result>> prod(PRODUCER_COUNT);
-    size_t base = 0;
-    for (size_t i = 0; i < PRODUCER_COUNT; ++i) {
-      size_t count = i < rem ? per_task + 1 : per_task;
-      prod[i] = producer(queue, count, base);
-      base += count;
-    }
-    auto startTime = std::chrono::high_resolution_clock::now();
-    std::vector<tmc::task<result>> cons(CONSUMER_COUNT);
-    cons[0] = consumer(queue, NELEMS);
-    auto c = tmc::spawn_many(cons).fork();
-    auto prodResults = co_await tmc::spawn_many(prod);
-    auto consResults = co_await std::move(c);
+             queue_t queue;
+             size_t per_task = NELEMS / PRODUCER_COUNT;
+             size_t rem = NELEMS % PRODUCER_COUNT;
 
-    auto endTime = std::chrono::high_resolution_clock::now();
+             // Construct producer and consumer tasks but don't initiate yet
+             std::vector<tmc::task<producer_result>> prod(PRODUCER_COUNT);
+             size_t base = 0;
+             for (size_t i = 0; i < PRODUCER_COUNT; ++i) {
+               size_t count = i < rem ? per_task + 1 : per_task;
+               prod[i] = producer(queue, count, base);
+               base += count;
+             }
+             std::vector<tmc::task<result>> cons(CONSUMER_COUNT);
+             cons[0] = consumer(queue, NELEMS);
 
-    size_t count = 0;
-    size_t sum = 0;
-    for (size_t i = 0; i < consResults.size(); ++i) {
-      count += consResults[i].count;
-      sum += consResults[i].sum;
-    }
-    if (count != NELEMS) {
-      std::printf(
-        "FAIL: Expected %zu elements but consumed %zu elements\n",
-        static_cast<size_t>(NELEMS), count
-      );
-    }
+             auto startTime = std::chrono::high_resolution_clock::now();
+             // Start consumers
+             auto c = tmc::spawn_many(cons).run_on(consEx).fork();
+             // Start producers and wait for them to finish
+             auto prodResults = co_await tmc::spawn_many(prod);
+             // Wait for consumers to finish
+             auto consResults = co_await std::move(c);
+             auto endTime = std::chrono::high_resolution_clock::now();
 
-    size_t expectedSum = 0;
-    for (size_t i = 0; i < NELEMS; ++i) {
-      expectedSum += i;
-    }
-    if (sum != expectedSum) {
-      std::printf("FAIL: Expected %zu sum but got %zu sum\n", expectedSum, sum);
-    }
-    size_t maxProducerDuration = 0;
-    for (size_t i = 0; i < prodResults.size(); ++i) {
-      if (maxProducerDuration < prodResults[i].duration_us) {
-        maxProducerDuration = prodResults[i].duration_us;
-      }
-    }
+             size_t count = 0;
+             size_t sum = 0;
+             for (size_t i = 0; i < consResults.size(); ++i) {
+               count += consResults[i].count;
+               sum += consResults[i].sum;
+             }
+             if (count != NELEMS) {
+               std::printf(
+                 "FAIL: Expected %zu elements but consumed %zu elements\n",
+                 static_cast<size_t>(NELEMS), count
+               );
+             }
 
-    size_t execDur = static_cast<size_t>(
-      std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime)
-        .count()
-    );
+             size_t expectedSum = 0;
+             for (size_t i = 0; i < NELEMS; ++i) {
+               expectedSum += i;
+             }
+             if (sum != expectedSum) {
+               std::printf(
+                 "FAIL: Expected %zu sum but got %zu sum\n", expectedSum, sum
+               );
+             }
+             size_t maxProducerDuration = 0;
+             for (size_t i = 0; i < prodResults.size(); ++i) {
+               if (maxProducerDuration < prodResults[i].duration_us) {
+                 maxProducerDuration = prodResults[i].duration_us;
+               }
+             }
 
-    double durMs = static_cast<double>(execDur) / 1000.0;
-    size_t elementsPerSec =
-      static_cast<size_t>(static_cast<double>(NELEMS) * 1000.0 / durMs);
-    std::printf(
-      "%zu prod\t%zu cons\t %.2f ms\t%s elements/sec\n", PRODUCER_COUNT,
-      CONSUMER_COUNT, durMs, formatWithCommas(elementsPerSec).c_str()
-    );
-    std::printf(
-      "producer max: %.2f ms\n",
-      static_cast<double>(maxProducerDuration) / 1000.0
-    );
+             size_t execDur = static_cast<size_t>(
+               std::chrono::duration_cast<std::chrono::microseconds>(
+                 endTime - startTime
+               )
+                 .count()
+             );
 
-    auto overallEnd = std::chrono::high_resolution_clock::now();
-    size_t overallDur =
-      static_cast<size_t>(std::chrono::duration_cast<std::chrono::microseconds>(
-                            overallEnd - overallStart
-      )
-                            .count());
-    double overallSec = static_cast<double>(overallDur) / 1000000.0;
-    std::printf("overall: %.2f sec\n", overallSec);
-    co_return 0;
-  }());
+             double durMs = static_cast<double>(execDur) / 1000.0;
+             size_t elementsPerSec = static_cast<size_t>(
+               static_cast<double>(NELEMS) * 1000.0 / durMs
+             );
+             std::printf(
+               "%zu prod\t%zu cons\t %.2f ms\t%s elements/sec\n",
+               static_cast<size_t>(PRODUCER_COUNT),
+               static_cast<size_t>(CONSUMER_COUNT), durMs,
+               formatWithCommas(elementsPerSec).c_str()
+             );
+             std::printf(
+               "producer max: %.2f ms\n",
+               static_cast<double>(maxProducerDuration) / 1000.0
+             );
+             co_return 0;
+           }()
+  )
+    .get();
 }
